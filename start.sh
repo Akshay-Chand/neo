90ce032472a623d4e41b86e752eac8e7f71256d3
Akshay-Chand <107416405+Akshay-Chand@users.noreply.github.com>
2022-07-21 10:13:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/Neo.git /Neo
+  git clone https://github.com/Akshay_Chand/Neo.git /Neo
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Neo
